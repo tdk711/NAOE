bdc8e270afd20b9f816b8ef53b1c0ff08f0dd778
Dilip Kumar <32437603+tdk711@users.noreply.github.com>
2022-02-22 12:50:10 +0530

    Update Cen_Dif.cpp

--- OE5450/Assgn_2/Cen_Dif.cpp
+++ OE5450/Assgn_2/Cen_Dif.cpp
@@ -7,18 +7,18 @@ using namespace std;
 void input_parser();
 
 // Global Variables:
-double X[61], Y[61];
+double X[41], Y[41];
 
 int main(){
 
     // Declare Variables
-    double ans[61][2]; 
+    double ans[41][2]; 
 
     input_parser(); // Parsing Input File to an Array
 
     
     // Calculating Derivatives:
-    for (int i = 1; i < 60; i++){
+    for (int i = 1; i < 40; i++){
 
         ans[i][0] = X[i]; 
         ans[i][1] = (Y[i+1] - Y[i-1])/(X[i+1] - X[i-1]);
@@ -28,14 +28,14 @@ int main(){
     ans[0][0] = 0.00;
     ans[0][1] = 0.00;
 
-    ans[60][0] = 20.00;
-    ans[60][1] = 0.00;
+    ans[40][0] = 20.00;
+    ans[40][1] = 0.00;
 
     // Writing Output to File:
     ofstream oStream;
     oStream.open("cubic_acc.txt");
 
-    for (int i = 0;  i < 61; i++)
+    for (int i = 0;  i < 41; i++)
     {
         oStream << ans[i][0] << " " << ans[i][1] << endl;
 
@@ -53,7 +53,7 @@ void input_parser(){
     ifstream iStream;
     iStream.open("Cubic_output.txt");
     
-    for (int i = 0;  i < 61; i++)
+    for (int i = 0;  i < 41; i++)
     {
         iStream >> number;
         X[i] = number;
@@ -65,3 +65,5 @@ void input_parser(){
     iStream.close();
     
 }
+
+
