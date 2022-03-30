b09e766fd081200cac7b3c15274d08e74320fb49
Dilip Kumar <32437603+tdk711@users.noreply.github.com>
2022-03-30 19:13:04 +0530

    Add files via upload

---
+++ OE5450/Assgn_5/ftcs_2_i.cpp
@@ -0,0 +1,89 @@
+///////////// OE5450 Assignment-5 ///////////////
+////// Solution of Linear Burgers' Equation ////
+////// Dilip Kumar NA18B004 | Spring 2022 //////
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    // Declare Variables
+    float Ct[2001], Ct1[2001], dx, dt;
+    const float u0 = 1.0;
+    const float alpha = 0.01;
+    int Nx, Nt, m;
+
+    // Magnitude of Discrete Step:
+    dt = 0.001;
+    dx = 0.01;
+
+    // Lengths:
+    Nx = static_cast<int>(20.0/dx);
+    Nt = static_cast<int>(3.0/dt);
+    
+
+    ofstream oStream;
+    oStream.open("output_3.txt");
+
+    // Initialising Array:
+    for (int i = 0;  i < 2001; i++)
+    {
+            Ct[i] = 0.0;
+            Ct1[i] = 0.0;
+    }
+
+    // Defining Constants:
+    const float c = (u0*dt)/(2.0*dx);
+    const float D = (alpha*dt)/(pow(dx,2));
+    cout<< c <<"  "<<D<<endl;
+
+    Ct[1000] = 1.0;
+    Ct1[1000] = 1.0;
+
+    for (int i = 0; i < 2001; i++){
+            oStream << Ct[i] << " ";
+        }
+    oStream<<endl;
+
+
+    // Solution:
+    for (int t = 1; t < 3000; t++){
+
+        for (int i = 0;  i < 2001; i++)
+        {
+            Ct[i] = Ct1[i];
+        }        
+
+
+        for (int i = 0; i < 2001; i++)
+        {
+            // Left Boundary:
+            if (i == 0){
+                Ct1[i] =  Ct[i+1]*(D - c) + (1 - 2*D)*Ct[i] + Ct[i+1]*(c + D);
+            }
+
+            // Right Boundary:
+            else if (i == 2000){
+                Ct1[i] =  Ct[i-1]*(D - c) + (1 - 2*D)*Ct[i] + Ct[i-1]*(c + D);
+            }
+
+            // Interior Elements:
+            else {
+                Ct1[i] =  Ct[i+1]*(D - c) + (1 - 2*D)*Ct[i] + Ct[i-1]*(c + D);
+            }
+
+            oStream << Ct1[i] << " ";
+        }
+        
+        oStream << endl;
+    }
+
+    // Writing Output to File:
+
+    
+    oStream.close();
+    cout<<"End";
+    return 0;
+}
